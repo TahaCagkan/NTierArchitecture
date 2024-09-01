f84c7c6757b819a5d158dd8897fd37af856074aa
Taha Cantürk <tahacagkan@outlook.com>
2024-09-01 14:45:12 +0300

    Add project files.

---
+++ NTierArchitecture.Entities/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace NTierArchitecture.Entities.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangeAsync(CancellationToken cancellationToken = default);
+    }
+}
